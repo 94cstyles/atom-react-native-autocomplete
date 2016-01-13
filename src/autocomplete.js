90cb64371dc567361bcce2cc709434b744d93dec
wenchao <wendaye0204@qq.com>
2016-01-13 13:30:19 +0800

    添加自动安装language-javascript-jsx插件
    添加自动安装language-javascript-jsx插件

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -1,9 +1,17 @@
 'use strict';
 
+import atomPackageDeps from 'atom-package-deps';
 import provider from "./provider";
 
 const autocomplete = {
-    activate: function(state) {},
+    activate: function(state) {
+        atomPackageDeps.install('language-javascript-jsx').then(() => {
+            atom.notifications.addSuccess('Success', {
+                detail: '\'language-javascript-jsx\' are installed success.',
+                dismissable: false
+            });
+        });
+    },
     deactivate: function() {},
     serialize: function() {},
     getProvider: function() {
